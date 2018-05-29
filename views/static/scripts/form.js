a1f382f16e9da0d0e2f327f96747334edf128752
Lee Gaines <eightlimbed@gmail.com>
2018-05-29 13:30:23 -0700

    adds github repo field

--- views/static/scripts/form.js
+++ views/static/scripts/form.js
@@ -10,6 +10,28 @@ $(document).ready(function () {
     }
   })
 
+  // Get GitHub repo
+  $('button#repo').on('click', function () {
+    event.preventDefault();
+    let val = $('#repo-input').val()
+    let len = 'https://github.com/'.length
+    let repo = val.slice(len)
+    let url = 'https://api.github.com/repos/' + repo
+    $.ajax ({
+      type: 'GET',
+      url: url,
+      success: function (data) {
+        console.log('success', data)
+      }
+    })
+
+    // Parse repo URL to remove slashes
+    val = val.split('/') // ["https:", "", "github.com", "user", "repo"]
+    val = val.slice(3)   // ["user", "repo"]
+    val = val.join(':')  // ["user:repo"]
+    containerSpecs['repo'] = val
+	})
+
   // Make a GET request to check API status
   $('button#status-check').click(function () {
     let url = '/api/v1/status'
@@ -24,7 +46,7 @@ $(document).ready(function () {
     let query = '';
     for (let key in containerSpecs) {
       let val = containerSpecs[key];
-      query += key + '=' + val
+      query += key + '=' + val + '&'
     }
     let url = '/api/v1/start/' + query
     $.get(url, function (data, status) {

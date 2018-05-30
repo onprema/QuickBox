5f56b082e2a141a97da1768ba9bc02a514676dcc
Lee Gaines <eightlimbed@gmail.com>
2018-05-30 14:23:13 -0700

    adds github repo baked into dockerfile

--- api/src/main.go
+++ api/src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -9,7 +8,6 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 
 	"github.com/gorilla/handlers"
@@ -36,73 +34,31 @@ func main() {
 }
 
 func removeContainer(w http.ResponseWriter, r *http.Request) {
-	// Gets container ID from the http Handler and removes the container
 
 	// Parse the query string to create the specs key/values
 	query := mux.Vars(r)["id"]
-  /*
-	specs, err := url.ParseQuery(query)
-	if err != nil {
-		panic(err)
-	}
-  */
-
   id := string(query)
 
 	// Remove the container
 	fmt.Printf("EXEC: docker rm -f %s\n", id)
-	_, err := exec.Command("docker", "rm", "-f", string(id)).Output()
+	_, err := exec.Command("docker", "rm", "-f", id).Output()
 	if err != nil {
 		panic(err)
 	}
+
+	// Remove the temporary image if there is one
+  if deadImage != "" {
+    fmt.Printf("EXEC: docker image rm -f %s\n", deadImage)
+    out, err := exec.Command("docker", "image", "rm", "-f", deadImage).Output()
+    if err != nil {
+      panic(err)
+    }
+    fmt.Printf("image rm out: %s\n", out)
+  }
 	json.NewEncoder(w).Encode("OK")
 }
 
-func buildImage(user string, repo string) string {
-	// Takes the github user/repo and returns the name of the image to
-  // runContainer
-
-	// Create buffers to read bytes and convert to strings
-	var tagBuffer bytes.Buffer
-	var imageBuffer bytes.Buffer
-
-	// Build the tag string
-	for i := 0; i < len(user); i++ {
-		tagBuffer.WriteByte(user[i])
-	}
-	tagBuffer.WriteString("/")
-	for i := 0; i < len(repo); i++ {
-		tagBuffer.WriteByte(repo[i])
-	}
-	// Image tag must be lowercase
-	tag := tagBuffer.String()
-	tag = strings.ToLower(tag)
-
-	// Build the image name string (default to :latest)
-	for i := 0; i < len(tag); i++ {
-		imageBuffer.WriteByte(tag[i])
-	}
-	imageBuffer.WriteString(":latest")
-	imageName := imageBuffer.String()
-
-	// Define the path to the Dockerfile of the base image
-	imagePath, _ := filepath.Abs("../../builds/ubuntu/14.04")
-
-	// Run the command
-	fmt.Printf("EXEC: docker build -t %s %s\n", tag, imagePath)
-	cmd := exec.Command("docker", "build", "-t", tag, imagePath)
-	err := cmd.Run()
-	if err != nil {
-		panic(err)
-	}
-
-	// Return the image name to runContainer() (ex: "my_name/my_repo:latest")
-	return imageName
-}
-
 func runContainer(w http.ResponseWriter, r *http.Request) {
-	// runContainer gets specifications from the http Handler and runs a
-	// a container based on those specs
 
 	// Parse the query string to create the specs key/values
 	query := mux.Vars(r)["specs"]
@@ -110,24 +66,24 @@ func runContainer(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+  base := string(specs["base"][0])
 
 	// If user entered a github repo...
-	var imageName string
-	if specs["repo"] != nil {
+  var imageName string
+	if specs["cloneURL"] != nil {
 
-		// repoUser and repoName has been split, like (my_name:my_repo)
-		repoUser := string(specs["repo"][0])
-		repoUser = strings.Split(repoUser, ":")[0]
-		repoName := string(specs["repo"][0])
-		repoName = strings.Split(repoName, ":")[1]
-		repoName = strings.ToLower(repoName)
+    repoName := string(specs["name"][0])
+    cloneURL := specs["cloneURL"][0]
+    path := makeDockerfile(base, cloneURL)
+    imageName = buildImage(path, repoName)
+    deadImage = imageName
 
-		// Call buildImage to create an image named "my_name/my_repo:latest"
-		// The container should include the cloned github repo
-		imageName = buildImage(repoUser, repoName)
-	} else {
-		imageName = string(specs["base"][0])
-	}
+  } else {
+
+    // Use the default image
+    imageName = string(specs["base"][0])
+
+  }
 
 	// Execute docker run ...
 	fmt.Printf("EXEC: docker run -itdP %s\n", imageName)
@@ -136,7 +92,7 @@ func runContainer(w http.ResponseWriter, r *http.Request) {
 		panic(runErr)
 	}
 
-	// Capture the hash and trim of trailing new line from output
+	// Capture the hash and trim ofs trailing new line from output
 	containerId := strings.TrimSpace(string(runOut))
 
 	// Get the port number of the container's hash
@@ -145,16 +101,15 @@ func runContainer(w http.ResponseWriter, r *http.Request) {
 	if portErr != nil {
 		panic(portErr)
 	}
-
-	// Get the last 5 characters of the output (the port number)
 	portNumberBytes := portOut[len(portOut)-6:]
 	portNumber := strings.TrimSpace(string(portNumberBytes))
 
 	type Container struct {
 		Id   string
 		Port string
+    Name string
 	}
-	container := Container{Id: containerId, Port: portNumber}
+  container := Container{Id: containerId, Port: portNumber, Name: imageName}
 
 	// Send container data back to the frontend
 	json.NewEncoder(w).Encode(container)

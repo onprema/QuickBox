cad6072aa2279111780f4de60cb96eb726d712ea
Lee Gaines <eightlimbed@gmail.com>
2018-06-01 16:10:35 -0700

    improves user experience on front end

--- views/static/scripts/repo.js
+++ views/static/scripts/repo.js
@@ -10,11 +10,22 @@ $(document).ready(function () {
     $.ajax ({
       type: 'GET',
       url: url,
+      statusCode: {
+        404: function() {
+          $('#repoFlash').delay(500).fadeIn('normal', function() {
+            $(this).delay(5000).fadeOut()
+          })
+        }
+      },
       success: function (data) {
+        $('#repoSuccess').delay(500).fadeIn('normal', function() {
+          $(this).css('display', 'block')
+        })
         cloneURL = data.clone_url
         // Replacing '/' with '|' to pass as url query string
         containerSpecs['cloneURL'] = cloneURL.replace(/\//g, '|')
         containerSpecs['name'] = data.name.toLowerCase()
+        containerSpecs['id'] = String(data.id)
         console.log('FROM GITHUB API: ', data)
       }
     })

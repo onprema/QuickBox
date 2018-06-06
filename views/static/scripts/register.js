162b717453574bcdabbbd0752c36301ccc51e399
Lee Gaines <eightlimbed@gmail.com>
2018-06-05 17:38:40 -0700

    updates frontend and improves UX

--- views/static/scripts/register.js
+++ views/static/scripts/register.js
@@ -2,12 +2,26 @@ let creds = {}
 $(document).ready(function () {
   // Hide service until users are verified
   $('.service').hide()
-  $('div#keyMsgSuccess').hide()
 
   // Username submit click
   $('button#username').click(function (event) {
     event.preventDefault();
-    console.log('here')
-    verifyGithubUsername();
+    $('.message p').text('Verifying GitHub username...')
+    let username = $('input#username').val()
+    let invalidChars = ['!', '@', '$', '%', '^', '&', '*', '(', ')']
+    for (let i = 0; i < invalidChars.length; i++) {
+      if (username.includes(invalidChars[i])) {
+        $('div#loader').hide()
+        $('.message p').text('Invalid GitHub username. Please try again.')
+        $('.register').show()
+      }
+    }
+    if (document.cookie.search(username) > -1 && username !== '') {
+          $('.service').show()
+          $('.message p').text('Welcome, ' + username + '!')
+    } else {
+      verifyGithubUsername();
+    }
+    $('.register').hide()
   })
 })

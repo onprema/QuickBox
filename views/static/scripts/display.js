d270d9b5c89b36515979319b1d7d5a9b2795d263
Lee Gaines <eightlimbed@gmail.com>
2018-06-05 18:19:22 -0700

    removes .com from ip addr

--- views/static/scripts/display.js
+++ views/static/scripts/display.js
@@ -7,7 +7,7 @@ function displaySwitch (data) {
   $('div#loader').hide()
 
   // Display Port and Destroy button after clicking 'start'
-  $('.cmd').append('ssh root@138.68.8.138.com -p ' + String(data.Port))
+  $('.cmd').append('ssh root@138.68.8.138 -p ' + String(data.Port))
   $('.running-container').append('<button id="destroy">Destroy</button>')
   $('.running-container').append('<p><span style="color: maroon; font-size: .85em">This container will automatically be destroyed in 12 hours.</p></span>')
 }

ab4143f94f9e8cda672eb2d8e0cb6f17a0159301
Lee Gaines <eightlimbed@gmail.com>
2018-06-07 15:08:50 +0000

    adds wsgi script for gunicorn

--- views/cod.py
+++ views/cod.py
@@ -7,15 +7,15 @@ from flask_cors import CORS, cross_origin
 import os
 
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
+application = Flask(__name__)
+application.url_map.strict_slashes = False
 host, port = '0.0.0.0', 5000
-cors = CORS(app, resources={r'/api/v1/*': {'origins': '*'}})
+cors = CORS(application, resources={r'/api/v1/*': {'origins': '*'}})
 
 
-@app.route('/')
+@application.route('/')
 def hello():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host=host, port=port, debug=True)
+    application.run(host=host, port=port, debug=True)

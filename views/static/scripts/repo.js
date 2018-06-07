86d3b26865b092a0e73d04ad8921f5dc367f19e4
Lee Gaines <eightlimbed@gmail.com>
2018-06-06 17:34:06 -0700

    removes unnecessary files

--- views/static/scripts/repo.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  
-  // Get GitHub repo (optional)
-  $('button#repo').on('click', function () {
-    event.preventDefault();
-	})
-})
--- views/static/scripts/status.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $('button#status-check').click(function () {
-    let url = '/api/v1/status'
-	$.get(url, function(data, status) {
-		console.log(data, status)
-	})
- })
-})

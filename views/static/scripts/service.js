8979b0fdc595f83aedfc8f76d9b89f1e7ce8cf09
Lee Gaines <eightlimbed@gmail.com>
2018-06-09 16:43:24 -0700

    refactor -- removes unused files

--- views/static/scripts/service.js
+++ views/static/scripts/service.js
@@ -1,6 +1,6 @@
+// Global variables
 let containerSpecs = {};
 let cloneURL = '';
-let containerHash = '';
 let creds = {};
 
 $(document).ready(function () {
@@ -15,41 +15,49 @@ $(document).ready(function () {
 
   // Make the default base ubuntu:14.04
   if (containerSpecs['base'] === undefined) {
-    containerSpecs['base'] = 'cod-ubuntu:14.04';
+    containerSpecs['base'] = 'qb-ubuntu:14.04';
   }
 
-  // Make a GET request to send container specs to API
+  // Make a GET request to send container specs to API when 'Start' is clicked
   $('input#start').click(function (event) {
     event.preventDefault();
-    $('.select-container').hide();
-    $('.nav').hide();
+    $('.service').hide();
+    $('.footer').hide();
     $('#loader').show();
 
     // Validate github repo
     let val = $('input#repo-input').val();
     if (val !== '') {
-      $('.message p').text('Validating repository...');
+
+      // Remove trailing slash, or `.git` if the enter clone URL
+      if (val[val.length-1] == '/') { val = val.slice(0, -1) };
+      if (val.slice(-4) == '.git') { val = val.slice(0, -4) };
+
+      $('.message p').text('Validating repository...')
+
       let len = 'https://github.com/'.length;
       let repo = val.slice(len);
       let url = 'https://api.github.com/repos/' + repo;
+
       $.ajax({
         type: 'GET',
         url: url,
         statusCode: {
-
-          // If repo is not valid...
+          // If repo is not valid GitHub API returns 404
           404: function () {
-            $('#repoFlash').delay(500).fadeIn('normal', function () {
-              $('#loader').hide();
-              $('.select-container').show();
-            });
+            $('#loader').hide();
+            $('.service').show();
+            $('.footer').show();
+            $('.message p').html('<p style="color: red">Invalid GitHub repository.</p>')
           }
         },
-
-        // If repo is valid...
+        // If repo is valid GitHub API returns 200
         success: function (data) {
-          // Add repo to containerSpecs
-          $('.message p').text('GitHub reposority validated.');
+          $('.message p').fadeIn('normal', function () {
+            $(this).html('<p style="color: green">GitHub repository validated.</p>')
+          });
+
+          // Add repo to containerSpecs (these are global variables)
           cloneURL = data.clone_url;
           // Replacing '/' with '|' to pass as url query string
           containerSpecs['cloneURL'] = cloneURL.replace(/\//g, '|');
@@ -57,13 +65,12 @@ $(document).ready(function () {
           containerSpecs['id'] = String(data.id);
 
           // Start build...
-          $('.select-container').hide();
-          $('.nav').hide();
-          $('#loader').show();
+          // startContainer is found in `functions.js`
           startContainer();
         }
       });
     } else {
+      // If they don't enter a repository just build the base image
       startContainer();
     }
   });
